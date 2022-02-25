68320684e8aa7cadd7323da7e9b26fb79ad80339
EmanBahrun <94019660+EmanBahrun@users.noreply.github.com>
2022-02-25 15:45:22 -0800

    Added flix files to repository

---
+++ Flix2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix2
+//
+//  Created by Eman Bahrun on 2/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var movieTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Flix2Tests/Pods-Flix2Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Flix2Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Flix2Tests
+@end
